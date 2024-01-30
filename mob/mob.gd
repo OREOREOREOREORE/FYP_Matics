685dc5fc4376d10bfa185eaf1cd7a1da543e2eb6
KTT1228 <sam12282010@gmail.com>
2024-01-30 20:50:44 +0800

    mod update

--- mob/mob.gd
+++ mob/mob.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
+@export var mob_scene: PackedScene
+@onready var player = get_node("../Player")
 
-const SPEED = 300.0
+var Speed = 100.0
 var HP = 100
 var def = 10
-var art = 10
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var atk = 10
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * Speed
 	move_and_slide()
