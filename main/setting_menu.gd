d9dbe1656038da46ce2857bfa057140608ee7a1e
KTT1228 <sam12282010@gmail.com>
2024-01-31 20:18:02 +0800

    add quit button

--- main/setting_menu.gd
+++ main/setting_menu.gd
@@ -7,11 +7,6 @@ signal start_game
 func _ready():
 	screen_size = get_viewport_rect().size
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_fullscreen_switch_toggled(toggled_on):
 	if toggled_on == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
