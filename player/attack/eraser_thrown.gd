0d60fb1f80c3d0355c8f7f05f36c9c41f5c2f4c3
OREOREOREOREORE <114270949+OREOREOREOREORE@users.noreply.github.com>
2024-02-29 20:52:12 +0800

    Player_attack_update

--- player/attack/eraser_thrown.gd
+++ player/attack/eraser_thrown.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 var HP = 1
-var SPD = 100
-var ATK = 50
+var SPD = 300
+var ATK = 10
 var KB = 100
 var ATK_Size = 1.0
 
@@ -15,10 +15,11 @@ var angle = Vector2.ZERO
 
 func _ready():
 	angle = global_position.direction_to(target)
-	rotation = angle.angle() + deg_to_rad(135)
+	rotation = angle.angle() + deg_to_rad(90)
 	var tween = create_tween()
 	tween.tween_property(self,"scale",Vector2(1,1)*ATK_Size,1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
 	tween.play()
+
 
 
 func _physics_process(delta):
@@ -26,4 +27,8 @@ func _physics_process(delta):
 
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
+
+
+func _on_area_entered(area):
 	queue_free()
