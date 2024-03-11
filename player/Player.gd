510e10b194271d20802c066624531578a919cd4f
OREOREOREOREORE <114270949+OREOREOREOREORE@users.noreply.github.com>
2024-03-11 15:26:50 +0800

    update_playerEXP

--- player/Player.gd
+++ player/Player.gd
@@ -4,11 +4,14 @@ extends CharacterBody2D
 @export var HP = 100
 @export var DEF = 0
 @export var ATK = 40
+
+@export var Level = 1
 @export var EXP = 0
-@export var Level = 0
+@export var EXP_required = 100
 
 @onready var player = $playerImg
 @onready var healthbar = get_node("PlayerUI/MarginContainer/HealthBar")
+@onready var player_ui = get_node("PlayerUI")
 var enemies = []
 
 signal dead
@@ -46,6 +49,10 @@ func take_damage(ATK_0: int):
 
 func update_healthbar():
 	healthbar.value = HP
+	#if HP >= 50:
+	#	healthbar.visible = false
+	#else:
+	#	healthbar.visible = true
 
 func _on_eraser_attack_timer_timeout():
 	if enemies.size() > 0:
@@ -70,11 +77,33 @@ func get_random_target():
 		return enemies.pick_random().global_position
 	else: return Vector2.UP
 	
-func exp_up(exp):
-	EXP += exp
+func exp_up(exp_gained):
+	var leveled = false
+	EXP += exp_gained
+	while EXP >= EXP_required:
+		EXP -= EXP_required
+		EXP_required = update_EXP_required()
+		level_up()
+		leveled = true
+	player_ui.UpdateExperiencebar(leveled)
 
+func level_up():
+	Level += 1
 
+func update_EXP_required():
+	var exp_cap = 0
+	if Level < 10:
+		exp_cap = Level * 100
+	elif Level < 20:
+		exp_cap = 124 * (Level - 19) * 6
+	else:
+		exp_cap = 255 * (Level-39) * 12
+		
+	return exp_cap 
 
+	
+	
+	
 
 
 
--- utility/player_ui.gd
+++ utility/player_ui.gd
@@ -1,11 +1,27 @@
 extends CanvasLayer
 
 
+@onready var player = get_node("../Player")
+@onready var experience_bar = get_node("ExperienceBar/VBoxContainer/ExperienceBar")
+@onready var expreience_bar_lable = get_node("ExperienceBar/VBoxContainer/ExperienceBar/Label")
+@onready var expreience_levelLable = get_node("ExperienceBar/VBoxContainer/Level") as Label
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SetExperiencebar()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func SetExperiencebar():
+	expreience_levelLable.set_text(str(player.Level))
+	experience_bar.max_value = player.EXP_required
+	experience_bar.value = player.EXP
+	expreience_bar_lable.set_text(str(player.EXP/player.EXP_required * 100) + " %")
+
+func UpdateExperiencebar(leveled):
+	if leveled == true:
+		experience_bar.max_value = player.EXP_required
