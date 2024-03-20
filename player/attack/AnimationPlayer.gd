355caecb2e63cd920b451abfe2e6519eaa38bc59
KTT1228 <sam12282010@gmail.com>
2024-03-20 21:32:23 +0800

    pan update

---
+++ player/attack/AnimationPlayer.gd
@@ -0,0 +1,13 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var animation: Animation = get_animation("att")
+	#print(animation.find_track("Pan:position", Animation.TYPE_VALUE))
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
